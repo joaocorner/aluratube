877e3edf0b707382fc18c9ebd632d771bb95f860
joaocorner <joaoeduardocorner@gmail.com>
2022-11-08 12:06:08 -0300

    importing config.json and updating home page

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,33 @@
+import config from "../config.json";
+
 function HomePage() {
-  return <div>Welcome to Next.js!</div>;
+  const estilosDaHomePage = { backgroundColor: "red" };
+  return (
+    <div style={estilosDaHomePage}>
+      <Menu />
+      <Header />
+      <Timeline />
+    </div>
+  );
 }
 
 export default HomePage;
+
+function Menu() {
+  return <div>Menu</div>;
+}
+
+function Header() {
+  return (
+    <div>
+      <img src="banner" />
+      <img src={`https://github.com/${config.github}.png`} />
+      {config.name}
+      {config.job}
+    </div>
+  );
+}
+
+function Timeline() {
+  return <div>Timeline</div>;
+}
