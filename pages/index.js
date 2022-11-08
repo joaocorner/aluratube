bde994993e301cfe1c0795a19a0c2e5b97d2b232
joaocorner <joaoeduardocorner@gmail.com>
2022-11-08 17:35:14 -0300

    updating timeline style

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import config from "../config.json";
 import styled from "styled-components";
 import { CSSReset } from "../src/components/CSSReset";
 import Menu from "../src/components/Menu";
+import { StyledTimeline } from "../src/components/Timeline";
 
 function HomePage() {
   const estilosDaHomePage = {
@@ -13,7 +14,14 @@ function HomePage() {
   return (
     <>
       <CSSReset />
-      <div style={estilosDaHomePage}>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          flex: 1,
+          //   backgroundColor: "red",
+        }}
+      >
         <Menu />
         <Header />
         <Timeline playlists={config.playlists}>Conteúdo</Timeline>
@@ -35,6 +43,7 @@ const StyledHeader = styled.div`
     border-radius: 50%;
   }
   .user-info {
+    margin-top: 50px;
     display: flex;
     align-items: center;
     width: 100%;
@@ -64,7 +73,7 @@ function Timeline(propriedades) {
   // Retorno por expressão
 
   return (
-    <div>
+    <StyledTimeline>
       {playlistNames.map((playlistName) => {
         const videos = propriedades.playlists[playlistName];
         console.log(playlistName);
@@ -87,6 +96,6 @@ function Timeline(propriedades) {
           </section>
         );
       })}
-    </div>
+    </StyledTimeline>
   );
 }
