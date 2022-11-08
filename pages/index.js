8f780508177ac4b2277495a1d1c2eb5a6a272014
joaocorner <joaoeduardocorner@gmail.com>
2022-11-08 12:14:37 -0300

    using styled-components and updating photo style

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import config from "../config.json";
+import styled from "styled-components";
 
 function HomePage() {
   const estilosDaHomePage = { backgroundColor: "red" };
@@ -17,14 +18,21 @@ function Menu() {
   return <div>Menu</div>;
 }
 
+const StyledHeader = styled.div`
+  img {
+    width: 80px;
+    height: 80px;
+    border-radius: 50%;
+  }
+`;
 function Header() {
   return (
-    <div>
-      <img src="banner" />
+    <StyledHeader>
+      {/* <img src="banner" /> */}
       <img src={`https://github.com/${config.github}.png`} />
       {config.name}
       {config.job}
-    </div>
+    </StyledHeader>
   );
 }
 
