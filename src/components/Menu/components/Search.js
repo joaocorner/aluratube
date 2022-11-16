18fe74e768e18b7fd8b97b0c40726ce6e0acfa94
joaocorner <joaoeduardocorner@gmail.com>
2022-11-16 14:46:42 -0300

    setting onChange

--- src/components/Menu/components/Search.js
+++ src/components/Menu/components/Search.js
@@ -35,9 +35,16 @@ const StyledSearch = styled.div`
 `;
 
 export default function Search() {
+  const [valorDaBusca, setValorDaBusca] = React.useState("Frost");
+  console.log("valorDaBusca", valorDaBusca);
   return (
     <StyledSearch>
-      <input type="text" />
+      <input
+        type="text"
+        onChange={(e) => {
+          setValorDaBusca(e.target.value);
+        }}
+      />
       <button>🔍</button>
     </StyledSearch>
   );
