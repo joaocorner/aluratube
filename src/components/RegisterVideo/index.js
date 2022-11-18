e98bd9078f15763a33dc4be8e89364ccfbbdb26b
joaocorner <joaoeduardocorner@gmail.com>
2022-11-18 15:07:19 -0300

    usestate to show and hidden form

--- src/components/RegisterVideo/index.js
+++ src/components/RegisterVideo/index.js
@@ -1,20 +1,30 @@
 import React from "react";
 import { StyledRegisterVideo } from "./styles";
 
-const index = () => {
+const RegisterVideo = () => {
+  const [formVisivel, setFormVisivel] = React.useState(false);
   return (
     <StyledRegisterVideo>
-      <button className="add-video">+</button>
-      <form>
-        <div>
-          <button className="close-modal">x</button>
-          <input placeholder="Título do vídeo" />
-          <input placeholder="URL" />
-          <button type="submit">Cadastrar</button>
-        </div>
-      </form>
+      <button className="add-video" onClick={() => setFormVisivel(true)}>
+        +
+      </button>
+      {formVisivel && (
+        <form>
+          <div>
+            <button
+              className="close-modal"
+              onClick={() => setFormVisivel(false)}
+            >
+              x
+            </button>
+            <input placeholder="Título do vídeo" />
+            <input placeholder="URL" />
+            <button type="submit">Cadastrar</button>
+          </div>
+        </form>
+      )}
     </StyledRegisterVideo>
   );
 };
 
-export default index;
+export default RegisterVideo;
