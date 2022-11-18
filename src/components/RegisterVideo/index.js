24f17ed42d6b923d9d0eaec2cc621aa63b2aa8de
joaocorner <joaoeduardocorner@gmail.com>
2022-11-18 17:09:58 -0300

    using onchange with form

--- src/components/RegisterVideo/index.js
+++ src/components/RegisterVideo/index.js
@@ -2,7 +2,9 @@ import React from "react";
 import { StyledRegisterVideo } from "./styles";
 
 const RegisterVideo = () => {
-  const [formVisivel, setFormVisivel] = React.useState(false);
+  const [formVisivel, setFormVisivel] = React.useState(true);
+  const [values, setValues] = React.useState({ titulo: "", url: "" });
+
   return (
     <StyledRegisterVideo>
       <button className="add-video" onClick={() => setFormVisivel(true)}>
@@ -17,8 +19,30 @@ const RegisterVideo = () => {
             >
               x
             </button>
-            <input placeholder="Título do vídeo" />
-            <input placeholder="URL" />
+            <input
+              placeholder="Título do vídeo"
+              value={values.titulo}
+              onChange={(e) => {
+                const value = e.target.value;
+                console.log(value);
+                setValues({
+                  ...values,
+                  titulo: value,
+                });
+              }}
+            />
+            <input
+              placeholder="URL"
+              value={values.url}
+              onChange={(e) => {
+                const value = e.target.value;
+                console.log(value);
+                setValues({
+                  ...values,
+                  url: value,
+                });
+              }}
+            />
             <button type="submit">Cadastrar</button>
           </div>
         </form>
