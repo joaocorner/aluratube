037bc2339f5336facbd717753d96f0335712ffe7
joaocorner <joaoeduardocorner@gmail.com>
2022-11-18 14:39:54 -0300

    showing modal

--- src/components/RegisterVideo/index.js
+++ src/components/RegisterVideo/index.js
@@ -2,7 +2,19 @@ import React from "react";
 import { StyledRegisterVideo } from "./styles";
 
 const index = () => {
-  return <StyledRegisterVideo></StyledRegisterVideo>;
+  return (
+    <StyledRegisterVideo>
+      <button className="add-video">+</button>
+      <form>
+        <div>
+          <button className="close-modal">x</button>
+          <input placeholder="Título do vídeo" />
+          <input placeholder="URL" />
+          <button type="submit">Cadastrar</button>
+        </div>
+      </form>
+    </StyledRegisterVideo>
+  );
 };
 
 export default index;
